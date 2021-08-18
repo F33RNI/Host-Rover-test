105b1859c9e070a8fa3a7bf8cfd5cca39ef9c707
Fern Hertz <xxoinvizionxx@gmail.com>
2021-08-19 00:26:09 +0300

    Update RPI-cam-test.py

--- RPI-cam-test.py
+++ RPI-cam-test.py
@@ -1,59 +1,21 @@
-import threading
-
 import cv2
-from flask import Flask, render_template, Response, redirect, request
+import numpy as np
 import socket
-app = Flask(__name__)
+import sys
+import pickle
+import struct
 
-frame = None
+cap=cv2.VideoCapture(1)
+clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.connect(('192.168.1.184',8089))
 
+while True:
+    ret,frame=cap.read()
+    # Serialize frame
+    data = pickle.dumps(frame)
 
-def opencv_video():
-    print('Starting openCV video')
-    global frame
-    capture = cv2.VideoCapture(1)
-    while True:
-        ret, frame = capture.read()
-        if not ret:
-            break
+    # Send message length first
+    message_size = struct.pack("L", len(data)) ### CHANGED
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/video_feed')
-def video_feed():
-    if frame is not None:
-        new_response = Response(gen(),
-                                mimetype='multipart/x-mixed-replace; boundary=frame')
-        new_response.headers.add('Access-Control-Allow-Origin', '*')
-        new_response.headers.add('Cache-Control', 'no-cache')
-        return new_response
-    else:
-        return '', 204
-
-
-def gen():
-    global frame
-    while True:
-        if frame is not None:
-            (flag, encoded_image) = cv2.imencode('.jpg', frame)
-            if not flag:
-                continue
-            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
-                   bytearray(encoded_image) + b'\r\n')
-        else:
-            break
-
-
-if __name__ == '__main__':
-    thread = threading.Thread(target=opencv_video)
-    thread.start()
-
-    local_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    local_socket.connect(("8.8.8.8", 80))
-    local_ip = local_socket.getsockname()[0]
-    # socket.gethostbyname(socket.gethostname())
-    app.run(host=local_ip, port='5000', debug=False)
+    # Then data
+    clientsocket.sendall(message_size + data)
